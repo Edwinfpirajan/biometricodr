bfcaee44da9d2de9cc72ba9f724d21215042a670
unknown <efpa1998@hotmail.com>
2023-02-09 12:09:12 -0500

    five testing sql

--- common/connection.go
+++ common/connection.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetConnection() *gorm.DB {
-	dsn := os.Getenv(" distriramirezcom_boreal:Developer2023++@tcp(107.6.54.56:3306)/distriramirezcom_boreal?charset=utf8mb4&parseTime=True&loc=Local")
+	dsn := os.Getenv("distriramirezcom_boreal:Developer2023++@tcp(107.6.54.56:3306)/distriramirezcom_boreal?charset=utf8mb4&parseTime=True&loc=Local")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
