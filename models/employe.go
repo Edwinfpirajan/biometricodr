bc15838e50ac217e5a20467900507709f90ac247
unknown <efpa1998@hotmail.com>
2023-02-17 12:36:44 -0500

    fixed

--- models/employe.go
+++ models/employe.go
@@ -1,14 +1,16 @@
 package models
 
+import "time"
+
 type Employe struct {
-	ID         int    `json:"id" gorm:"primary_key;auto_increment"`
-	PinEmploye string `json:"pinEmploye" gorm:"FOREIGNKEY:PinEmploye" `
-	FirstName  string `json:"first_name" `
-	LastName   string `json:"last_name"`
-	Company    string `json:"company"`
-	Position   string `json:"position"`
-	ScheduleId int    `json:"schedule_id"`
-	// CreatedAt  time.Time `json:"fechacreacion"`
+	ID         int       `json:"id" gorm:"primary_key;auto_increment"`
+	PinEmploye string    `json:"pinEmploye" gorm:"FOREIGNKEY:PinEmploye" `
+	FirstName  string    `json:"first_name" `
+	LastName   string    `json:"last_name"`
+	Company    string    `json:"company"`
+	Position   string    `json:"position"`
+	ScheduleId int       `json:"schedule_id"`
+	CreatedAt  time.Time `json:"fechacreacion"`
 }
 
 type EmployeeWithSchedule struct {
