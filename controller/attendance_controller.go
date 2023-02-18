5058b9e20e2025f0248912c917b9033a8965510c
Edwinfpirajan <88447582+Edwinfpirajan@users.noreply.github.com>
2023-02-17 20:11:01 -0500

    Update attendance_controller.go

--- controller/attendance_controller.go
+++ controller/attendance_controller.go
@@ -14,7 +14,7 @@ import (
 
 func SaveRegisterAttendance(c echo.Context) error {
 	db := common.GetConnection()
-	defer common.CloseDB(db)
+	
 
 	var attendance entity.Attendance
 	err := c.Bind(&attendance)
@@ -143,7 +143,7 @@ func SaveRegisterAttendance(c echo.Context) error {
 
 func GetAllAttendance(c echo.Context) error {
 	db := common.GetConnection()
-	defer common.CloseDB(db)
+	
 	attendance := []models.GetAllAttendances{}
 
 	db.Table("attendances").Select("*").Joins("INNER JOIN employes e on e.pin_employe = attendances.pin_employe_fk").Find(&attendance)
@@ -155,7 +155,7 @@ func GetAllAttendance(c echo.Context) error {
 
 func ValidateHorary(c echo.Context) error {
 	db := common.GetConnection()
-	defer common.CloseDB(db)
+	
 
 	body, err := ioutil.ReadAll(c.Request().Body)
 	if err != nil {
@@ -179,7 +179,7 @@ func ValidateHorary(c echo.Context) error {
 func ValidateEmploye(c echo.Context) error {
 	id := c.Param("pin")
 	db := common.GetConnection()
-	defer common.CloseDB(db)
+	
 
 
 	var employe models.Employe
