9dee63e7651f39dc473b00bbcd5e6d2ed0167675
unknown <efpa1998@hotmail.com>
2023-02-20 12:24:17 -0500

    devv

--- main.go
+++ main.go
@@ -17,7 +17,13 @@ func main() {
 	// e.Logger.Fatal(e.Start(":8080"))
 
 	PORT, _ := strconv.Atoi(os.Getenv("PORT"))
+
+	if PORT == 0 {
+		PORT = 8080
+	}
 	HOST := os.Getenv("SERVER_HOST")
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%d", HOST, PORT)))
 }
+
+//PORT="8080" HOST="0.0.0.0" go run .
