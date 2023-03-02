b173803027441b7695d448cf3c3bc4ddb90d7e3e
unknown <efpa1998@hotmail.com>
2023-03-02 11:40:07 -0500

    migrate db

--- common/connection.go
+++ common/connection.go
@@ -16,7 +16,8 @@ func getConnection() *gorm.DB {
 	// port := os.Getenv("MYSQLPORT")
 	// dsn := fmt.Sprintf("root:%s@tcp(%s:%s)/railway?charset=utf8mb4&parseTime=True&loc=Local", pass, host, port)
 	// dsn := "biometrico:_u7825Son@tcp(serverpruebas.tk:3306)/biometricov?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "server_bio:72*4xceG4@tcp(185.68.110.162:3306)/server_bio?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "server_bio:72*4xceG4@tcp(185.68.110.162:3306)/server_bio?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:KMBh7xfi44n0ZNfDcdei@tcp(containers-us-west-170.railway.app:6926)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
